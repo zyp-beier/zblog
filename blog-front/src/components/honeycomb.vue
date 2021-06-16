04679956b0aa3d717a8cd3990a65e0f6906518e2
zyp-beier <1528466340@qq.com>
2021-06-16 16:44:21 +0800

    蜂巢图

--- blog-front/src/components/honeycomb.vue
+++ blog-front/src/components/honeycomb.vue
@@ -1,120 +1,34 @@
 <template>
-  <div style="background-color:#eee; padding: 50px">
-    <div class="box">
-      <a href="#">
-        <div class="rotate ">
-            <div class="left common"></div>
-            <div class="right common"></div>
+  <div class="hive">
+    <div class="main">
+      <div class="list">
+        <div class="left item" v-for="(item, index) in 5" :key="index" :class="item">
+            <div class="right">
+              <a href="#"><img src="../assets/img/20181124175204_217.jpg" alt=""></a>
+            </div>
         </div>
-      </a>
-      <a href="#">
-          <div class="rotate ">
-              <div class="left common"></div>
-              <div class="right common"></div>
-          </div>
-      </a>
-      <a href="#">
-          <div class="rotate ">
-              <div class="left common"></div>
-              <div class="right common"></div>
-          </div>
-      </a>
-      <a href="#">
-          <div class="rotate ">
-              <div class="left common"></div>
-              <div class="right common"></div>
-          </div>
-      </a>
-      <a href="#">
-          <div class="rotate ">
-              <div class="left common"></div>
-              <div class="right common"></div>
-          </div>
-      </a>
-    </div>
-    <div class="box">
-      <a href="#">
-          <div class="rotate ">
-            <div class="left common"></div>
-            <div class="right common"></div>
-          </div>
-      </a>
-      <a href="#">
-          <div class="rotate ">
-            <div class="left common"></div>
-            <div class="right common"></div>
-          </div>
-      </a>
-      <a href="#">
-          <div class="rotate ">
-              <div class="left common"></div>
-              <div class="right common"></div>
-          </div>
-      </a>
-      <a href="#">
-          <div class="rotate ">
-              <div class="left common"></div>
-              <div class="right common"></div>
-          </div>
-      </a>
-    </div>
-    <div class="box">
-      <a href="#">
-        <div class="rotate ">
-            <div class="left common"></div>
-            <div class="right common"></div>
+      </div>
+      <div class="list even">
+        <div class="left item" v-for="(item, index) in 4" :key="index" :class="item">
+            <div class="right">
+              <a href="#"><img src="../assets/img/20181124175205_100.jpg" alt=""></a>
+            </div>
         </div>
-      </a>
-      <a href="#">
-          <div class="rotate ">
-              <div class="left common"></div>
-              <div class="right common"></div>
-          </div>
-      </a>
-      <a href="#">
-          <div class="rotate ">
-              <div class="left common"></div>
-              <div class="right common"></div>
-          </div>
-      </a>
-      <a href="#">
-          <div class="rotate ">
-              <div class="left common"></div>
-              <div class="right common"></div>
-          </div>
-      </a>
-      <a href="#">
-          <div class="rotate ">
-              <div class="left common"></div>
-              <div class="right common"></div>
-          </div>
-      </a>
-    </div>
-    <div class="box">
-      <a href="#">
-          <div class="rotate ">
-            <div class="left common"></div>
-            <div class="right common"></div>
-          </div>
-      </a>
-      <a href="#">
-          <div class="rotate ">
-            <div class="left common"></div>
-            <div class="right common"></div>
-          </div>
-      </a>
-      <a href="#">
-          <div class="rotate ">
-              <div class="left common"></div>
-              <div class="right common"></div>
-          </div>
-      </a>
-      <a href="#">
-          <div class="rotate ">
-              <div class="left common"></div>
-              <div class="right common"></div>
-          </div>
-      </a>
+      </div>
+      <div class="list">
+        <div class="left item" v-for="(item, index) in 5" :key="index" :class="item">
+            <div class="right">
+              <a href="#"><img src="../assets/img/20181124175530_602.jpg" alt=""></a>
+            </div>
+        </div>
+      </div>
+      <div class="list even">
+        <div class="left item" v-for="(item, index) in 4" :key="index" :class="item">
+            <div class="right">
+              <a href="#"><img src="../assets/img/20181124175204_217.jpg" alt=""></a>
+            </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -124,57 +38,58 @@ export default {
   name: "honeycomb",
   data() {
     return {
+      hiveList: 18
     }
   }
 }
 </script>
 
 <style scoped>
-  .box{
-    display: flex;
-    justify-content:center;
+  .hive {
+    background-color: #eee;
   }
-  .rotate{
-    position:relative;
-    height:110px;
-    width:190px;
-    margin:32px 5px;
-    background:url('../assets/img/overViewBg.jpg');
-    background-size: contain;
-    background-size:auto 220px;
-    background-position: -15px -55px;
-    transition: all .5s;
+  .main {
+    width: 70%;
+    padding: 2rem;
+    margin: 0 auto;
+  }
+  .left {
+    width:10rem;
+    height:12rem;
+    transform: rotate(120deg);
+    overflow: hidden;
+  }
+  .right {
+    height: 100%;
+    width: 100%;
+    transform: rotate(-60deg);
+    overflow: hidden;
+    /* box-shadow: inset 0 0 0 200px rgb(36 36 36 / 70%); */
+  }
+  .left .right img {
+    width: 100%;
+    height: 100%;
+    transform: rotate(-60deg);
+    overflow: hidden;
+  }
+  .item:hover {
+    /* -webkit-box-shadow: inset 0 0 0 200px rgb(36 36 36 / 70%);
+    box-shadow: inset 0 0 0 200px rgb(36 36 36 / 70%); */
     transform: scale(1);
-  }
-  .rotate:hover{
-    transform: scale(1.05);
     transition: all .3s ease-in-out;
   }
-  .common{
-    position: absolute;
-    height:100%;
-    width:100%;
-    overflow: hidden;
-    left:0;
+  .list {
+    display: flex;
+    margin-top: -2rem;
   }
-  .common:before{
-    content:'';
-    position: absolute;
-    background:url('../assets/img/overViewBg.jpg') 50% 50% no-repeat;
-    background-size:auto 220px;
-    width:190px;
-    height:220px;
+  .list:nth-child(1) {
+    margin: 0;
   }
-  .left{
-    transform: rotate(60deg)
+  .even {
+    margin-left: 5.5rem;
   }
-  .left:before{
-    transform: rotate(-60deg) translate(48px,-28px)
-  }
-  .right{
-    transform: rotate(-60deg);
-  }
-  .right:before{
-    transform: rotate(60deg) translate(-48px,-28px)
+  .item {
+    margin-right: 1.2rem;
+    transition: all .5s;
   }
 </style>
