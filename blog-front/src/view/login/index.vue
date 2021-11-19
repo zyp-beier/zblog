c1c8e3304c5583718451aeac6fa9b92601cac9f7
zyp-beier <1528466340@qq.com>
2021-11-19 11:30:45 +0800

    modify style

--- blog-front/src/view/login/index.vue
+++ blog-front/src/view/login/index.vue
@@ -1,27 +1,26 @@
 <template>
   <div class="login-page">
-    <div class="login-wrap">
-      <div class="login-title">博客管理后台</div>
-      <div class="userlogin">
-        <!-- <div class="userlogin-top">用户登录</div> -->
-        <div class="border"></div>
-        <div class="userlogin-bottom">user login</div>
-      </div>
-      <div class="login-input">
-        <el-form :model="loginForm" :rules="rules" ref="ruleForm">
-          <el-form-item label="用户名" prop="username" style="color: #fff">
-            <el-input
-              placeholder="请输入用户名"
-              v-model="loginForm.username"
-            ></el-input>
-          </el-form-item>
-          <el-form-item label="密码" prop="password" >
-            <el-input placeholder="请输入密码" v-model="loginForm.password" show-password @keyup.enter="login('ruleForm')"></el-input>
-          </el-form-item>
-          <el-form-item class="login">
-            <el-button @click="login('ruleForm')">登录</el-button>
-          </el-form-item>
-        </el-form>
+    <div class="login-bg">
+      <div class="login-dialog">
+        <div class="login-dialog-left">
+          <div class="login-dialog-title">
+            <div class="login-dialog-title-hello">Hello,</div>
+            <div class="login-dialog-title-back">Welcome back!</div>
+          </div>
+          <el-form :model="loginForm" :rules="rules" ref="ruleForm">
+            <el-form-item label="Username" prop="username" style="color: #fff">
+              <el-input placeholder="Place enter user name" v-model="loginForm.username"></el-input>
+            </el-form-item>
+            <el-form-item label="Password" prop="password" >
+              <el-input placeholder="Place enter password" v-model="loginForm.password" show-password @keyup.enter="login('ruleForm')"></el-input>
+            </el-form-item>
+            <el-form-item class="login">
+              <el-button @click="login('ruleForm')">login</el-button>
+              <el-button @click="login('ruleForm')">login up</el-button>
+            </el-form-item>
+          </el-form>
+        </div>
+        <div class="login-dialog-right"><img src="../../assets/img/login-right1.png" alt=""></div>
       </div>
     </div>
   </div>
@@ -39,12 +38,12 @@ export default {
         password: ""
       },
       rules: {
-        username: [
-          {required: true, message: '请输入用户名', trigger: 'blur'},
-        ],
-        password: [
-          {required: true, message: '请输入密码',trigger: 'blur'}
-        ]
+        // username: [
+        //   {required: true, message: '请输入用户名', trigger: 'blur'},
+        // ],
+        // password: [
+        //   {required: true, message: '请输入密码',trigger: 'blur'}
+        // ]
       }
     }
   },
@@ -78,54 +77,112 @@ export default {
   }
 }
 </script>
+<style>
+  .login-dialog .el-form-item .el-form-item__label {
+    font-size: 18px;
+    font-weight: bold;
+    color: black;
+  }
+  .el-input .el-input__inner {
+    border: none;
+    border-radius: 0;
+    border-bottom: 1px solid black;
+    padding: 0;
+  }
+  .el-button--default {
+    width: 100px;
+    border: 1px solid #2e186c;
+    color: #2e186c;
+    border-radius: 0;
+  }
+  .el-button--default:nth-child(1) {
+    background-color: #2e186c;
+    color: #fff;
+  }
+  .el-button:nth-child(1):focus, .el-button:nth-child(1):hover {
+    background-color: #2e186c;
+    color: #fff;
+    border: 1px solid #2e186c;
+  }
+  .el-button:nth-child(2):focus, .el-button:nth-child(2):hover {
+    background-color: #fff;
+    color: #2e186c;
+    border: 1px solid #2e186c;
+  }
+</style>
 
 <style scoped>
-  .login-input >>> .el-form-item__label {
-      color: #fff !important;
-    }
   .login-page {
     height: 100vh;
     width: 100%;
+    min-width: 900px;
     background-color: #3CA992;
     position: relative
   }
-  .login-wrap {
+  .login-bg {
+    width: 100%;
+    height: 100vh;
+    background-color: #290950;
+    position: relative;
+  }
+  .login-bg::before{
+    content: "";
     position: absolute;
-    top: 50%;
+    bottom: 0;
+    left: 0;
+    width: 50%;
+    height: 45vh;
+    background-color: #2e186c;
+  }
+  .login-bg::after{
+    content: "";
+    position: absolute;
+    top: 0;
+    right: 0;
+    width: 50%;
+    height: 45vh;
+    background-color: #2e186c;
+  }
+  .login-dialog {
+    width: 900px;
+    height: 500px;
+    position: absolute;
+    top: 0;
+    bottom: 0;
     left: 0;
     right: 0;
-    transform: translate(0, -50%);
-    margin: 0 auto;
-    width: 20rem;
+    margin: auto;
+    background-color: #fff;
+    z-index: 99;
+    display: flex;
   }
-  .login-title {
-    text-align: center;
-    font-size: 2rem;
-    font-family: 'ZCOOL XiaoWei', serif;
-    color: #fff;
-    margin: 0 auto 1rem auto;
+  .login-dialog-left {
+    width: 450px;
+    padding: 40px;
+    box-sizing: border-box;
   }
-  .userlogin {
-    color: #fff;
-    text-align: center;
-    font-family: 'ZCOOL XiaoWei', serif;
-    margin-bottom: 1rem;
+  .login-dialog-title {
+    margin-bottom: 30px;
+
   }
-  .border {
+  .login-dialog-title-hello {
+    font-size: 30px;
+    color: #2e186c;
+  }
+  .login-dialog-title-back {
+    font-size: 35px;
+    font-weight: bold;
+    color: #290950;
+  }
+
+  .login-dialog-right {
+    width: 450px;
+
+  }
+  .login-dialog-right img {
+    height: 500px;
     width: 100%;
-    height: 1px;
-    background: -webkit-linear-gradient(left, #3CA992 -4%,#fff 50%,#3CA992 100%);
-    margin: .5rem auto;
+    object-fit: cover;
   }
-  .userlogin-top {
-    font-size: 1.6rem;
-  }
-  .login-input {
-    width: 80%;
-    margin: 0 auto 1rem auto;
-    line-height: 3rem;
-  }
-  .login {
-    text-align: center;
-  }
+
 </style>
